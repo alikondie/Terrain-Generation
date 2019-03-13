ead6c5d8f3a62fefb4dc0b806e0a4973aaf0ec8a
Alikondie <alikondie@gmail.com>
2019-03-13 10:19:40 +0100

    initial commit

---
+++ Assets/Scripts/GenHeightMap.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenHeightMap : MonoBehaviour
+{
+
+    public int mapWidth = 1280;
+    public int mapHeight = 720;
+    public float mapScale = 0.2f;
+    public float[,] map;
+
+    public Renderer texture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float[,] shit = generateNoise();
+        map = generateNoise();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public float[,] generateNoise()
+    {
+        float[,] map = new float[mapWidth, mapHeight];
+        if (mapScale <= 0)
+            mapScale = Mathf.Epsilon;
+
+        for(int j = 0; j < mapHeight; j++)
+        {
+            for(int i = 0;i < mapWidth; i++)
+            {
+                float x = i / mapScale;
+                float y = j / mapScale;
+
+                //float perlin = Mathf.Lerp(0.0f,1.0f,Mathf.PerlinNoise(x, y));
+                float perlin = Mathf.PerlinNoise(x, y);
+                //Debug.Log(perlin);
+                map[i, j] = perlin;
+            }
+        }
+
+        return map;
+    }
+
+    public void generateMap()
+    {
+        float[,] map = generateNoise();
+
+        Texture2D tex = new Texture2D(mapWidth, mapHeight);
+
+        Color[] grayScaleMap = new Color[mapWidth * mapHeight];
+        for (int j = 0; j < mapHeight; j++)
+        {
+            for (int i = 0; i < mapWidth; i++)
+            {
+                grayScaleMap[j * mapWidth + i] = Color.Lerp(Color.black, Color.white, map[i, j]);
+            }
+        }
+
+        tex.SetPixels(grayScaleMap);
+        tex.Apply();
+        texture.sharedMaterial.mainTexture = tex;
+        texture.transform.localScale = new Vector3(mapWidth, 1, mapHeight);
+
+    }
+}
---
+++ Assets/Scripts/MeshCreation.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MeshCreation : MonoBehaviour
+{
+
+    Mesh mesh;
+    public GenHeightMap generatedMap;
+    public Texture2D heightMap;
+    public Material mat;
+
+    int width,height;
+    float[,] map;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.AddComponent<MeshFilter>();
+        width = generatedMap.mapWidth;
+        height = generatedMap.mapHeight;
+        map = generatedMap.map;
+        mesh = GetComponent<MeshFilter>().mesh;
+        importMapFromTexture();
+
+        generateMap();
+    }
+
+    // Update is called once per frame
+    void importMapFromTexture()
+    {
+        width = heightMap.width;
+        height = heightMap.height;
+        Debug.Log(width);
+        map = new float[width, height];
+        for (int i = 0; i < heightMap.width; i++)
+        {
+            for(int j = 0; j < heightMap.height; j++)
+            {
+                float pixel = heightMap.GetPixel(i, j).grayscale;
+                map[i, j] = pixel;
+            }
+        }
+    }
+
+    // remplir les triangles
+    public void generateMap()
+    {
+        Vector3[] verts = new Vector3[width * height];
+        int[] triangles = new int[(height - 1) * (width - 1) * 6];
+        for(int j = 0; j < height; j++)
+            for(int i = 0; i < width;i++)
+            {
+                verts[j * width + i] = new Vector3(i,map[i,j]*10,j); 
+            }
+        int f = 0;
+        for(int i = 0;i < (width-1) * height; i++)
+        {
+            if ((i + 1) % height == 0 || (i + 1) % width == 0)
+                continue;
+
+            
+            triangles[f] = i;
+            f++;
+            triangles[f] = width + i;
+            f++;
+            triangles[f] = width + i + 1;
+            f++;
+            triangles[f] = i;
+            f++;
+            triangles[f] = width + i + 1;
+            f++;
+            triangles[f] =  i + 1;
+            f++;
+        }
+
+        mesh.vertices = verts;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+}
